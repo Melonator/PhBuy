fa5e4cc4f4a6bf6ba7fad423b81fd86cb66bf73e
Johndayll <johndayllearizala@yahoo.com>
2020-12-11 22:04:17 +0800

    Add foundation for query

---
+++ PhBuy/DataAnalytics.Designer.cs
@@ -0,0 +1,82 @@
+﻿
+namespace PhBuy
+{
+    partial class DataAnalytics
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.bunifuElipse1 = new Bunifu.Framework.UI.BunifuElipse(this.components);
+            this.generalChart = new LiveCharts.WinForms.CartesianChart();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // bunifuElipse1
+            // 
+            this.bunifuElipse1.ElipseRadius = 5;
+            this.bunifuElipse1.TargetControl = this;
+            // 
+            // generalChart
+            // 
+            this.generalChart.BackColor = System.Drawing.Color.White;
+            this.generalChart.Location = new System.Drawing.Point(28, 151);
+            this.generalChart.Name = "generalChart";
+            this.generalChart.Size = new System.Drawing.Size(847, 348);
+            this.generalChart.TabIndex = 0;
+            this.generalChart.Text = "cartesianChart1";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(28, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(847, 49);
+            this.panel1.TabIndex = 1;
+            // 
+            // DataAnalytics
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(909, 700);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.generalChart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "DataAnalytics";
+            this.Text = "DataAnalytics";
+            this.Load += new System.EventHandler(this.DataAnalytics_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Bunifu.Framework.UI.BunifuElipse bunifuElipse1;
+        private System.Windows.Forms.Panel panel1;
+        private LiveCharts.WinForms.CartesianChart generalChart;
+    }
+}
---
+++ PhBuy/DataAnalytics.cs
@@ -0,0 +1,72 @@
+﻿using LiveCharts;
+using LiveCharts.Wpf;
+using PhBuyModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Media;
+
+namespace PhBuy
+{
+    public partial class DataAnalytics : Form
+    {
+        private PhBuyContext _data = new PhBuyContext();
+        private List<Products> _products;
+        private List<Orders> _orders;
+        private Seller _currentSeller;
+        private int _id = 65376;
+
+        private LineSeries sales = new LineSeries();
+        public DataAnalytics()
+        {
+            InitializeComponent();
+        }
+
+        private void DataAnalytics_Load(object sender, EventArgs e)
+        {
+            _products = _data.Products.Where(i => i.SellerId == _id).ToList();
+            _orders = _data.Orders.Where(i => i.SellerId == _id).ToList();
+
+            ChartValues<double> values = new ChartValues<double>();
+            List<string> months = new List<string>();
+
+            var something = _orders.Select(i => new { i.DateOrdered, i.TotalPrice })
+                .GroupBy(a => a.DateOrdered.Value.ToString("MMMM"), (key, group) => new
+                {
+                    sales = group.Sum(k => k.TotalPrice),
+                    month = key
+                }).ToList();
+
+            foreach(var o in something)
+            {
+                values.Add((double)o.sales);
+                months.Add(o.month);
+            }
+
+            generalChart.AxisY.Add(new Axis
+            {
+                Title = "Income",
+                LabelFormatter = value => value.ToString("C")
+            });
+
+            generalChart.AxisX.Add(new Axis
+            {
+                Title = "Month",
+                Labels = months
+            }); 
+
+            sales.Values = values;
+
+            generalChart.Series.Add(sales);
+
+     
+
+        }
+    }
+}
